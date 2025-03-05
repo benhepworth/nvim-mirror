a551f18b557337a492cad6e2f9d249c28241045a
Ben Hepworth <b.hepworth@cablelabs.com>
2025-03-05 12:27:59 -0700

    Update configs, add co-pilot to nvim

--- lua/bhepworth/plugins/formatting.lua
+++ lua/bhepworth/plugins/formatting.lua
@@ -21,11 +21,6 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
@@ -37,3 +32,9 @@ return {
     end, { desc = "Format file or range (in visual mode)" })
   end,
 }
+
+--      format_on_save = {
+--        lsp_fallback = true,
+--        async = false,
+--        timeout_ms = 1000,
+--      },
--- lua/bhepworth/plugins/init.lua
+++ lua/bhepworth/plugins/init.lua
@@ -1,4 +1,17 @@
 return {
     "nvim-lua/plenary.nvim", -- lua functions that many plugins use
     "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+
+    -- Add Copilot:
+    {
+      "github/copilot.vim",
+      -- event = "InsertEnter",  -- Lazy-load on first insert
+      config = function()
+        -- Optional: disable tab key to avoid conflicts
+        vim.g.copilot_no_tab_map = true
+  
+        -- Example: accept suggestion with <C-J>
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      end
+    },
 }
